1becb27a04ad8a033064e4f25eec3cbc6158f4f6
Tobias Hermann <editgym@gmail.com>
2016-07-10 20:14:11 +0200

    buttons

--- src/js/htmlmain.js
+++ src/js/htmlmain.js
@@ -17,8 +17,4 @@ function init() {
   elmContent = Elm.DivineOrBenign.embed(mainDiv);
   // It seems to need the delay to work with Elm 0.17.1. I have no idea why.
   setTimeout(function(){ elmContent.ports.languagePort.send(language); }, 10);
-
-  $(".btn").mouseup(function(){
-    $(this).blur();
-})
 }
